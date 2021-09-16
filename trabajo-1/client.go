9bd655000ab3a8f8936f246994f44f23756f5af7
rtolosana <36404195+rtolosana@users.noreply.github.com>
2021-09-16 11:15:01 +0200

    Add files via upload

--- trabajo-1/client.go
+++ trabajo-1/client.go
@@ -4,7 +4,7 @@
 *			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
 * FECHA: septiembre de 2021
 * FICHERO: client.go
-* DESCRIPCIÓN: cliente por completar para el trabajo 1
+* DESCRIPCIÓN: cliente completo para los cuatro escenarios de la práctica 1
 */
 package main
 
@@ -36,20 +36,6 @@ func main(){
 
     conn, err := net.DialTCP("tcp", nil, tcpAddr)
     checkError(err)
-    defer conn.Close()
-    
-    // TODO completar el código para que el cliente envíe el intervalo al servidor
-    // y el servidor de devuelva los primos que hay dentro del intervalo
 
     // la variable conn es de tipo *net.TCPconn
-    // net.TCPconn es un tipo de dato que permite realizar la comunicación TCP / IP full duplex
-    // en Golang. Entre otros TCPconn tiene dos métodos que se utilizan tanto por
-    // el cliente como por el servidor:
-    //
-    // Write escribe en el canal TCP de comunicación los datos codificados en b
-    // func (c *TCPConn) Write(b []byte) (n int, err os.Error)
-    //
-    // Read lee del canal los datos y los guarda en b
-    // func (c *TCPConn) Read(b []byte) (n int, err os.Error)
-    
 }
