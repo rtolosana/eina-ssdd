49d2243f2df5326828c5ecf434616db2c7ca9c5e
rtolosana <36404195+rtolosana@users.noreply.github.com>
2021-10-22 02:34:58 +0200

    Add files via upload

---
+++ practica3/com/definitions.go
@@ -0,0 +1,15 @@
+/*
+* AUTOR: Rafael Tolosana Calasanz
+* ASIGNATURA: 30221 Sistemas Distribuidos del Grado en Ingeniería Informática
+*			Escuela de Ingeniería y Arquitectura - Universidad de Zaragoza
+* FECHA: septiembre de 2021
+* FICHERO: definitions.go
+* DESCRIPCIÓN: contiene las definiciones de estructuras de datos necesarias para
+*			la práctica 3
+*/
+package com
+
+type TPInterval struct {
+    A int
+    B int
+}
